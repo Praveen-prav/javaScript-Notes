640ff99d94960fcee00c479f9a2bcf5c3312f3cc
Praveen-prav <praveenpravee406@gmail.com>
2025-11-13 13:14:37 +0530

    Revert "Revert "Reverted to commit ID 9f11b2e""
    This reverts commit 938877edd06b5e662c3ddc25c89691c434fc5652.

--- functions.js
+++ functions.js
@@ -2,14 +2,9 @@ function add(a, b){
     return a + b
 }
 
-function subtraction(a, b){
-    return a - b
-}
-
 a = 2
 b = 5
 
 sum = add(a, b)
 
 console.log(`addition of a and b is ${sum}`)
-console.log(`subtraction of a and b is ${subtraction(a,b)}`)
